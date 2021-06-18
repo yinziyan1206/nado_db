bff1d875658437ac62d84ca35b1423643e1653c5
yinziyan1206 <408856732@qq.com>
2021-06-18 11:52:21 +0800

    [fix] mongodb connect v1.0.9 stable

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 from .driver import MySQL, PostgreSQL, SqlServer, MongoDB
-from .aiodriver import AioMySQL, AioPostgreSQL
+from .aiodriver import AioMySQL, AioPostgreSQL, AioMongoDB
 from .utils import QueryWrapper
 __author__ = 'ziyan.yin'
-__version__ = '1.0.8'
+__version__ = '1.0.9'
 
-__all__ = ['MySQL', 'PostgreSQL', 'SqlServer', 'QueryWrapper', 'AioMySQL', 'AioPostgreSQL', 'MongoDB']
+__all__ = ['MySQL', 'PostgreSQL', 'SqlServer', 'QueryWrapper', 'AioMySQL', 'AioPostgreSQL', 'MongoDB', 'AioMongoDB']
