4e7a959c283b66149e25035d3e3ff08fc775f3e1
yinziyan1206 <408856732@qq.com>
2021-09-10 13:19:15 +0800

    [fix] query wrapper include int bugs

--- __init__.py
+++ __init__.py
@@ -3,6 +3,6 @@ from .driver import MySQL, PostgreSQL, SqlServer, MongoDB
 from .aiodriver import AioMySQL, AioPostgreSQL, AioMongoDB
 from .utils import QueryWrapper
 __author__ = 'ziyan.yin'
-__version__ = '1.1.12'
+__version__ = '1.1.13'
 
 __all__ = ['MySQL', 'PostgreSQL', 'SqlServer', 'QueryWrapper', 'AioMySQL', 'AioPostgreSQL', 'MongoDB', 'AioMongoDB']
